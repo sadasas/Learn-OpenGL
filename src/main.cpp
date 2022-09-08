9f8f24b9142698c59907667e7b90ded756dbfc23
wahyusrp <setio101199@gmail.com>
2022-09-09 01:17:16 +0700

    Draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <config.h>
 
-
-int main(void)
+int main(int argc, char* argv[])
 {
+    std::cout << argv[0] << " Version "<< PROJECT_VERSION_MAJOR << " " << PROJECT_VERSION_MINOR << "\n";
+
+	//init window 
     GLFWwindow* window;
     
-    /* Initialize the library */
+    //Initialize the library 
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    //Create a windowed mode window and its OpenGL context 
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -17,19 +21,28 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+    //Make the window's context current 
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    //Loop until the user closes the window 
     while (!glfwWindowShouldClose(window))
     {
-        /* Render */
-        glClear(GL_COLOR_BUFFER_BIT);
+    // Render 
 
-        /* Swap front and back buffers */
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        //draw triangle
+        glBegin(GL_TRIANGLES);
+        glVertex2d(-0.5f,-0.5f);
+        glVertex2d(0.0f,0.5f);
+        glVertex2d(0.5f,-0.5f);
+        glEnd();
+
+
+        //Swap front and back buffers 
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        //Poll for and process events 
         glfwPollEvents();
     }
 
