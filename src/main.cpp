e8a3b05b660d04f2611959ca1ebf26db436ab093
wahyusrp <setio101199@gmail.com>
2022-09-09 14:58:49 +0700

    Draw triangle : implement vertex buffer and default shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
+#include <gl/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <config.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    std::cout << argv[0] << " Version "<< PROJECT_VERSION_MAJOR << " " << PROJECT_VERSION_MINOR << "\n";
+    std::cout << argv[0] << " Version " << PROJECT_VERSION_MAJOR << " " << PROJECT_VERSION_MINOR << "\n";
 
-	//init window 
-    GLFWwindow* window;
-    
-    //Initialize the library 
+    // init window
+    GLFWwindow *window;
+
+    // Initialize the library
     if (!glfwInit())
         return -1;
 
-    //Create a windowed mode window and its OpenGL context 
+    // Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -21,28 +22,45 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    //Make the window's context current 
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    //Loop until the user closes the window 
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "error";
+    }
+
+    //create triangle pos
+    float positions[6] ={
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+    };
+    //create vertex buffer
+    unsigned int buffer;
+    glGenBuffers(1,&buffer);
+    glBindBuffer(GL_ARRAY_BUFFER,buffer);
+    glBufferData(GL_ARRAY_BUFFER,6 * sizeof(float),positions,GL_STATIC_DRAW);
+    
+    //create vertex atribute
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2 ,0);
+
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-    // Render 
+        // Render
 
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        //draw triangle
-        glBegin(GL_TRIANGLES);
-        glVertex2d(-0.5f,-0.5f);
-        glVertex2d(0.0f,0.5f);
-        glVertex2d(0.5f,-0.5f);
-        glEnd();
 
+        // shaders
+        glDrawArrays(GL_TRIANGLES,0,3);
+    
 
-        //Swap front and back buffers 
+        // Swap front and back buffers
         glfwSwapBuffers(window);
 
-        //Poll for and process events 
+        // Poll for and process events
         glfwPollEvents();
     }
 
