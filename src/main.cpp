f7389f18e101dc844a95fd3cade6656c844196e4
wahyusrp <setio101199@gmail.com>
2022-09-21 23:36:04 +0700

    write simple shader

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,32 @@
 #include <iostream>
 #include <config.h>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id,1,&src,nullptr);
+    glCompileShader(id);
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER,vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER,fragmentShader);
+
+    glAttachShader(program,vs);
+    glAttachShader(program,fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << argv[0] << " Version " << PROJECT_VERSION_MAJOR << " " << PROJECT_VERSION_MINOR << "\n";
@@ -45,6 +71,31 @@ int main(int argc, char *argv[])
     //create vertex atribute
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2 ,0);
+
+    //create shader
+    std::string vertexShader = 
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) in vec4 position;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = position;\n"
+    "}\n";
+
+     std::string fragmentShader = 
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) out vec4 color;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(1.0,0.0,0.0,1.0);\n"
+    "}\n";
+
+    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    glUseProgram(shader);
+
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
